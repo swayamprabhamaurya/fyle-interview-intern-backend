7ceef4fe95ca5af27cad9bcb877c255874320a06
Swayam Prabha Maurya <142203394+swayamprabhamaurya@users.noreply.github.com>
2024-07-18 02:08:38 +0530

    Update number_of_assignments_per_state.sql

--- tests/SQL/number_of_assignments_per_state.sql
+++ tests/SQL/number_of_assignments_per_state.sql
@@ -1,1 +1,7 @@
+-- Write query to get number of graded assignments for each student:
+-- tests/SQL/number_of_assignments_per_state.sql
+SELECT state, COUNT(id) AS assignment_count
+FROM assignments
+WHERE state IN ('DRAFT', 'GRADED', 'SUBMITTED')
+GROUP BY state;
 
